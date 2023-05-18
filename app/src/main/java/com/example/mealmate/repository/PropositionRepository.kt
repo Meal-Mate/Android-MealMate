c501519b95e4ef9d366e7f50a86c08b3bc198c8c
AlaEddineBenRejab <alaeddine.benrejab@esprit.tn>
2023-05-18 23:11:10 +0100

    addInterests

--- app/src/main/java/com/example/mealmate/repository/PropositionRepository.kt
+++ app/src/main/java/com/example/mealmate/repository/PropositionRepository.kt
@@ -1,16 +1,20 @@
 package com.example.mealmate.repository
 import com.example.mealmate.data.api.methods.PropositionApi
+import com.example.mealmate.data.api.methods.PropositionsApi
+import com.example.mealmate.data.api.request.GetRestaurantMapsRequest
 import com.example.mealmate.data.api.request.PropostionRequest
+import com.example.mealmate.data.api.response.GetRestaurantMapsResponse
 import com.example.mealmate.data.api.response.PropositionResponse
 import retrofit2.Response
 
 class PropositionRepository {
     suspend fun AddProposition(propostionRequest: PropostionRequest): Response<PropositionResponse>? {
         return  PropositionApi.getApi()?.AddProposition(propostionRequest = propostionRequest)
-            suspend fun GetRestaurantsMaps(getRestaurantMapsRequest: GetRestaurantMapsRequest): Response<GetRestaurantMapsResponse>? {
-        return  PropositionsApi.getApi()?.getRestaurants(getRestaurantMapsRequest = getRestaurantMapsRequest)
-    }
+
     
+    }
+    suspend fun GetRestaurantsMaps(getRestaurantMapsRequest: GetRestaurantMapsRequest): Response<GetRestaurantMapsResponse>? {
+        return  PropositionsApi.getApi()?.getRestaurants(getRestaurantMapsRequest = getRestaurantMapsRequest)
     }
 }
 
